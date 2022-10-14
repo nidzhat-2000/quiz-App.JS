b53746b0a41a3b35d8c26c1ea0565099bee34f6b
nidzhat-2000 <nidzhat.niyazov@gmail.com>
2022-10-14 14:12:39 +0400

    improved

--- script.js
+++ script.js
@@ -16,6 +16,12 @@ const answer4 = document.querySelector('.fourth');
 const allAnswers = [answer1, answer2, answer3, answer4];
 
 /*
+answerTab.forEach(el =>
+  el.addEventListener('click', e => console.log(e.target.innerHTML))
+);
+*/
+
+/*
 /////////////////////////////////////////////////////////
 // Timer Function ⤵
 timer.innerHTML = '00:30';
@@ -56,12 +62,15 @@ const fetchQuestion = async () => {
 
   ////////////////////////////////////////
   const question = data.datas[`${place}`].question;
+  // console.log(question)
   const answers = data.datas[`${place}`].answers;
+  // console.log(answers);
   const first = data.datas[`${place}`].answers[0];
   const second = data.datas[`${place}`].answers[1];
   const third = data.datas[`${place}`].answers[2];
   const fourth = data.datas[`${place}`].answers[3];
   correct = data.datas[`${place}`].answers[data.datas[`${place}`].correctAns];
+  // console.log(correct);
 
   questionTab.innerHTML = question;
   answer1.innerHTML = first;
@@ -73,9 +82,10 @@ const fetchQuestion = async () => {
 };
 fetchQuestion();
 
-allAnswers.forEach(ab =>
+// allAnswers.forEach(ab =>
+answerTab.forEach(ab =>
   ab.addEventListener('click', e => {
-    if (ab.innerHTML == correct) {
+    if (e.target.innerHTML == correct) {
       result++;
 
       /////////////////////////////
@@ -98,60 +108,86 @@ allAnswers.forEach(ab =>
 const questionDelayer = () => {
   setTimeout(() => {
     fetchQuestion();
-    timerFun();
+    // timerFun();
   }, 500);
 };
 */
 
+/**/
+function pick(n, min, max) {
+  var values = [],
+    i = max;
+  while (i >= min) 
+  values.push(i--);
+  // console.log(values);
+  var results = [];
+  var maxIndex = max;
+  for (i = 1; i <= n; i++) {
+    maxIndex--;
+    var index = Math.floor(maxIndex * Math.random());
+    // console.log(index);
+    results.push(values[index]);
+    values[index] = values[maxIndex];
+  }
+  return results;
+}
+
+
+const randomNums = pick(15, 0, 15);
+console.log(randomNums);
+const randomAns = pick(4,0,4)
+console.log(randomAns);
+
 let place = 0;
 let result = 0;
 let correct;
-// let interval
 
-const intervalizer = () => {
-  const interval = Math.round(Math.random() * (7 - 0)) + 0;
-  console.log(interval);
 
-  const fetchQuestion = async () => {
-    const response = await fetch('./data.json');
-    const data = await response.json();
-    // Fetch Part ⤴
+const fetchQuestion = async () => {
+  const response = await fetch('./data.json');
+  const data = await response.json();
+  // Fetch Part ⤴
 
-    // Finishh game ⤵
-    if (place == data.datas.length) {
-      app.classList.add('blur');
-      endGame.style.opacity = '1';
-      return;
-    }
+  // Finishh game ⤵
+  if (place == randomNums.length) {
+    app.classList.add('blur');
+    endGame.style.opacity = '1';
+    return;
+  }
+  console.log(randomNums);
+  // console.log(randomNums[place]);
+  // console.log(`${randomNums[place]}`);
 
-    ////////////////////////////////////////
-    const question = data.datas[`${interval}`].question;
-    const answers = data.datas[`${interval}`].answers;
-    const first = data.datas[`${interval}`].answers[0];
-    const second = data.datas[`${interval}`].answers[1];
-    const third = data.datas[`${interval}`].answers[2];
-    const fourth = data.datas[`${interval}`].answers[3];
-    correct =
-      data.datas[`${interval}`].answers[data.datas[`${interval}`].correctAns];
+  ////////////////////////////////////////
+  // console.log(`${randomNums[place]}`);
+  const question = data.datas[`${randomNums[place]}`].question;
+  // const answers = data.datas[`${randomNums[place]}`].answers;
+  const first = data.datas[`${randomNums[place]}`].answers[`${randomAns[0]}`];
+  const second = data.datas[`${randomNums[place]}`].answers[`${randomAns[1]}`];
+  const third = data.datas[`${randomNums[place]}`].answers[`${randomAns[2]}`];
+  const fourth = data.datas[`${randomNums[place]}`].answers[`${randomAns[3 ]}`];
+  correct =
+    data.datas[`${randomNums[place]}`].answers[
+      data.datas[`${randomNums[place]}`].correctAns
+    ];
+    console.log(correct);
 
-    questionTab.innerHTML = question;
-    answer1.innerHTML = first;
-    answer2.innerHTML = second;
-    answer3.innerHTML = third;
-    answer4.innerHTML = fourth;
+  questionTab.innerHTML = question;
+  answer1.innerHTML = first;
+  answer2.innerHTML = second;
+  answer3.innerHTML = third;
+  answer4.innerHTML = fourth;
 
-    place++;
-  };
-
-  fetchQuestion();
+  place++;
+  // console.log(place);
 };
-intervalizer();
+fetchQuestion();
 
 allAnswers.forEach(ab =>
   ab.addEventListener('click', e => {
     if (ab.innerHTML == correct) {
       result++;
-      console.log(result);
+      // console.log(result);
       /////////////////////////////
       // Result indicator ⤵
       const percentage = (result / 15) * 100;
@@ -171,11 +207,28 @@ allAnswers.forEach(ab =>
 
 const questionDelayer = () => {
   setTimeout(() => {
-    intervalizer();
-    // fetchQuestion();
+    // intervalizer();
+    fetchQuestion();
     // timerFun();
   }, 500);
 };
 
+
 // Restart game ⤵
 restart.addEventListener('click', e => window.location.reload());
+
+// console.log(pick(15, 0, 15));
+
+function randomArrayShuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+var alphabet=["a","b","c","d","e"];
+// console.log( randomArrayShuffle(alphabet) );; 
